46b958da2dd2e4f168e257bf990510562bb29d3f
vladi <vladi.konov@gmail.com>
2023-11-28 23:35:03 +0200

    automatin last finish

--- tests/gmail_login_test.py
+++ tests/gmail_login_test.py
@@ -1,3 +1,4 @@
+import time
 from typing import Dict, Any
 
 import pytest
@@ -6,30 +7,35 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-@pytest.mark.parametrize("username, password, expected_result", [
-    ("student", "Password123", "success")
-])
+@pytest.mark.parametrize("userna:me, password, expected_result", [
+    ("", "Password123", "failure"),
+    ("student", "", "failure"),
+    ("none", "none", "failure"),
+    ("student", "Password123", "success")])
 def test_login(username, password, expected_result):
     result = login(username, password)
     assert result == expected_result
 
 
 def login(username, password):
-    cap: Dict[str, Any] ={
+    caps: Dict[str, Any] = {
         'platformName': 'Android',
         'deviceName': 'Android',
         "browserName": "Chrome",
-        "automationName": "UiAutomator2",
-        "chromedriverExecutable": "/path/to/chromedriver"
-    # 'appPackage': 'com.hmh.api',
-        #  'appActivity': '.ApiDemos'
-        # 'language': 'en',
-        # 'locale': 'US'
+        "automationName": "UiAutomator2"
     }
+    # ios_caps = {
+    #     'platformName': 'iOS',
+    #     'deviceName': 'iOS',
+    #     "browserName": "Safari",
+    #     'automationName': 'XCUITest'
+    # }
+
+    # caps = android_caps if username.lower() == 'android' else ios_caps
 
     appiumUrl = 'http://localhost:4723/wd/hub'
     loginUrl = "https://practicetestautomation.com/practice-test-login/"
-    driver = webdriver.Remote(appiumUrl, options=AppiumOptions().load_capabilities(cap))
+    driver = webdriver.Remote(appiumUrl, options=AppiumOptions().load_capabilities(caps))
 
     driver.get(loginUrl)
 
@@ -38,9 +44,16 @@ def login(username, password):
 
     password_input = driver.find_element(By.ID, 'password')
     password_input.send_keys(password)
+    time.sleep(3)
+    # submit_button = WebDriverWait(driver, 10).until(
+    #     EC.visibility_of_element_located((By.ID, 'submit'))
+    # )]
+    print("Attempting to click on the submit button")
 
     submit_button = driver.find_element(By.ID, 'submit')
     submit_button.click()
+    print("Clicked on the submit button successfully")
+    time.sleep(7)
 
     result = "success" if driver.find_elements(By.LINK_TEXT, "Log out") else "failure"
     driver.quit()
